fa8f0e0d50e0170fed9adb927c5d39fbaa5adff7
lihenrodrigues <rolien.rosa@otimagestao.com>
2023-05-25 11:38:10 -0300

    Adicionando Listagem de Produtos na Home

--- FrontEnd/GeekShopping.Web/Controllers/ProductController.cs
+++ FrontEnd/GeekShopping.Web/Controllers/ProductController.cs
@@ -21,11 +21,9 @@ public class ProductController : Controller
         _productService = productService ?? throw new ArgumentNullException(nameof(productService));
     }
 
-    [Authorize]
     public async Task<IActionResult> ProductIndex()
     {
-        var token = await HttpContext.GetTokenAsync("access_token");
-        var products = await _productService.FindAllProducts(token);
+        var products = await _productService.FindAllProducts("");
         return View(products);
     }
 
